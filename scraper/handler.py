dbae84eac4d843614d8b385866c516f3405029c9
viktorfa <vikfand@gmail.com>
2019-09-06 23:30:22 +0200

    Make it possible to configure crawls better.

--- scraper/handler.py
+++ scraper/handler.py
@@ -1,24 +1,76 @@
 import json
+from datetime import datetime, timedelta
+import logging
+
+from launch_fargate import launch_fargate
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def run_crawlers(event, context):
+    result = []
+    for x in filter(should_crawl, get_crawler_config()):
+        try:
+            if x.get("run_in_lambda"):
+                # TODO Implement function to invoke crawl in new Lambda function
+                pass
+            else:
+                fargate_launch_response = launch_fargate(dict(
+                    spider_name=x.get("spider_name"),
+                    spider_kwargs=x.get("spider_kwargs"),
+                ), {})
+                result.append(fargate_launch_response)
+        except Exception:
+            logging.exception(
+                f"Could not launch spider {x.get('spider_name')}"
+            )
+    return result
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def get_crawler_config():
+    # This function could make an API call to a CMS like AirTable,
+    # Strapi or DynamoDB.
+    return [
+        {
+            "spider_name": "header_spider",
+            "spider_kwargs": {
+                "start_urls": ["https://example.com"],
+            },
+            "previous_crawl": {
+                "success_state": True,
+                "items_crawled": 100,
+                "finish_date": datetime.now() - timedelta(days=3),
+            },
+        },
+        {
+            "spider_name": "header_spider",
+            "spider_kwargs": {
+                "start_urls": ["https://www.ietf.org"],
+            },
+            "previous_crawl": {
+                "success_state": True,
+                "items_crawled": 100,
+                "finish_date": datetime.now() - timedelta(hours=16),
+            },
+            "crawl_interval_hours": 12,
+            "settings": {
+                "AUTOTHROTTLE_ENABLED": True,
+            }
+        },
+        {
+            "spider_name": "header_spider",
+            "spider_kwargs": {
+                "start_urls": ["https://bitcoin.org"],
+            },
+        },
+        {
+            "spider_name": "other_spider",
+            "previous_crawl": None
+        },
+    ]
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def should_crawl(x):
+    previous_crawl = x.get("previous_crawl")
+    if previous_crawl:
+        time_interval_hours = x.get("crawl_interval_hours", 24*7)
+        return previous_crawl.get("finish_date") + timedelta(hours=time_interval_hours) < datetime.now() or not previous_crawl.get("success_state")
+    return True
--- scraper/my_sls_scraper/spiders/header_spider.py
+++ scraper/my_sls_scraper/spiders/header_spider.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from w3lib.html import remove_tags
 
 from scrapy.spiders import CrawlSpider, Rule
@@ -9,6 +10,18 @@ class HeaderSpider(CrawlSpider):
 
     start_urls = ["https://scrapy.org"]
     allowed_domains = ["scrapy.org"]
+
+    def __init__(self, **kwargs):
+        # Enables overriding attributes for a flexible spider
+        if kwargs.get("start_urls"):
+            self.start_urls = kwargs.get("start_urls")
+            self.allowed_domains = list(
+                urlparse(x).hostname for x in self.start_urls
+            )
+
+        super().__init__(
+            **{k: v for k, v in kwargs.items() if not hasattr(self, k)}
+        )
 
     rules = [  # Get all links on start url
         Rule(
